13e9ef19df91b7a5ce4eb7ec466433f29602bd49
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-07-12 22:14:23 +0530

    Add files via upload

---
+++ Stacks/NextGEL.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector <int> v;// 1 3 2 4
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(2);
+    v.push_back(4);
+    vector <int> ans(4);
+    stack <int> s;
+    for(int i=0;i<v.size();i++){
+        if(!s.empty() && s.top()>v[i])
+            ans[i]=s.top();
+        while(!s.empty() && s.top()<=v[i])
+            s.pop();
+        if(s.empty())
+            ans[i]=-1;
+        else    
+            ans[i]=s.top();
+
+        s.push(v[i]);
+    }
+    for(int i:ans)
+    cout<<i<<" ";
+    cout<<endl;
+}
