cfd16aa91fc73a1206d6eff61254c8ec4d4dcc8c
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-07-15 22:21:35 +0530

    Create Min Stack O(n).cpp

---
+++ Stacks/Min Stack O(n).cpp
@@ -0,0 +1,29 @@
+class MinStack {
+public:
+    stack <int> s;
+    stack <int> ss;
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        s.push(val);
+        if(ss.empty() || ss.top()>=val)
+            ss.push(val);
+    }
+    
+    void pop() {
+        if(s.empty())
+            return;
+        s.pop();
+        ss.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return ss.top();
+    }
+};
