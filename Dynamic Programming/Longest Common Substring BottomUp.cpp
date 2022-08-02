3a296080dcba63253d61908214a70ec1c572e7bf
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-08-02 21:06:05 +0530

    Create Longest Common Substring BottomUp.cpp

---
+++ Dynamic Programming/Longest Common Substring BottomUp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t[101][101];
+int lcsubstring(string x,string y,int m,int n){
+    for(int i=0;i<m+1;i++){
+        for(int j=0;j<n+1;j++){
+            if(i==0||j==0)
+                t[i][j]=0;
+        }
+    }
+    for(int i=1;i<m+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(x[i-1]==y[j-1])
+                t[i][j]=1+t[i-1][j-1];
+            else    
+                t[i][j]=max(t[i-1][j],t[i][j-1]);
+        }
+    }
+    return t[m][n];
+}
+int main(){
+    string x="ababcde";
+    string y="abcde";
+    int m=x.length();
+    int n=y.length();
+    cout<<lcsubstring(x,y,m,n)<<endl;
+}
