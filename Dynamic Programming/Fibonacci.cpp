bc873e1938de251d9430a6aa4912a5a4a3cf231f
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-09-26 16:53:38 +0530

    Update Fibonacci.cpp

--- Dynamic Programming/Fibonacci.cpp
+++ Dynamic Programming/Fibonacci.cpp
@@ -3,7 +3,7 @@ int fib(int n) {
         if(n==1 || n==0)
             return n;
         t[0]=0;
-        t[1]=1;
+        t[1]= 1;
         for(int i=2;i<n+1;i++){
             t[i]=t[i-1]+t[i-2];
         }
