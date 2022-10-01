c7d66df513e109645f9c3ff7d7731a73afc4a78b
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-10-02 05:02:38 +0530

    Update Kth Smallest Element.cpp

--- Heap-Priority Queue/Kth Smallest Element.cpp
+++ Heap-Priority Queue/Kth Smallest Element.cpp
@@ -7,7 +7,7 @@ int main(){
     priority_queue<int> maxh;
     for(int i=0;i<n;i++){
         maxh.push(arr[i]);
-        if(maxh.size()>k){
+        if(maxh.size() >k){
             maxh.pop();
         }
     }
